d134de6141b582c2a8169cc743253d96d807bb19
Matt Ross <amsross@gmail.com>
2023-03-21 10:46:03 -0400

    fix: Correctly deserialize HdmiInput

--- core/sdk/src/api/firebolt/fb_hdmi.rs
+++ core/sdk/src/api/firebolt/fb_hdmi.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde::{de::Error, Deserialize, Deserializer, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GetAvailableInputsResponse {
@@ -9,5 +9,37 @@ pub struct GetAvailableInputsResponse {
 pub struct HdmiInput {
     pub id: i32,
     pub locator: String,
-    pub connected: String,
+    #[serde(deserialize_with = "string_to_bool")]
+    pub connected: bool,
+}
+
+fn string_to_bool<'de, D>(deserializer: D) -> Result<bool, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: &str = Deserialize::deserialize(deserializer)?;
+
+    match s {
+        "true" => Ok(true),
+        "false" => Ok(false),
+        _ => Err(Error::unknown_variant(s, &["true", "false"])),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::HdmiInput;
+
+    #[test]
+    fn test_hdmi_input_deserialize() {
+        let json_str = r#"{"id":3,"locator":"my locator","connected":"false"}"#;
+        let test_struct = serde_json::from_str::<HdmiInput>(&json_str);
+
+        assert!(test_struct.is_ok());
+
+        let result = test_struct.unwrap();
+        assert_eq!(result.id, 3);
+        assert_eq!(result.locator, "my locator");
+        assert_eq!(result.connected, false);
+    }
 }
